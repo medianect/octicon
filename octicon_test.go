f2bc239e412a4af0352d7f231903467b5b558325
Xvdfe <xvdfe@medianect.com>
2020-10-22 11:52:57 +0200

    Fixed undeclared variable usage.

--- octicon_test.go
+++ octicon_test.go
@@ -10,7 +10,7 @@ import (
 func Example() {
 	str := octicon.Icon("alert", 28, 28)
 	if str == "" {
-		log.Fatalln(err)
+		log.Fatalln("Icon doesn't exist.")
 	}
 	fmt.Printf("%s", str)
 	// Output:
